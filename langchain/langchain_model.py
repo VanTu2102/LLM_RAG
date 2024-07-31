fc65e27d6032f959db44c73ae67a66fc37e9b906
Hoàng Tú <vantu2102@gmail.com>
2024-07-31 11:05:00 +0700

    .

--- langchain/langchain_model.py
+++ langchain/langchain_model.py
@@ -6,13 +6,14 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_API_KEY"] = "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"
-os.environ["OPENAI_API_KEY"] = "sk-None-yRdDXOeUslynDkzNZQUmT3BlbkFJKrcvPdOgEhPVhuTYESD0"
+os.environ["OPENAI_API_KEY"] = "sk-proj-MHpGTFRaPeNFTaEaGWHFT3BlbkFJABUMjlLmcKTWa5th45rm"
 
 model = chat_models.ChatOpenAI(model="gpt-4")
-
 messages = [
-    SystemMessage(content="Translate the following from English into Italian"),
-    HumanMessage(content="hi!"),
+    (
+        "system",
+        "You are a helpful translator. Translate the user sentence to French.",
+    ),
+    ("human", "I love programming."),
 ]
-
 print(model.invoke(messages))
