2eab845f6a25f2e03359d269f8f9e3244dec850e
Hoàng Tú <vantu2102@gmail.com>
2024-07-31 17:24:37 +0700

    .

--- langchain/langchain_model.py
+++ langchain/langchain_model.py
@@ -1,19 +1,27 @@
 import getpass
 import os
 
-from langchain_openai import chat_models
-from langchain_core.messages import HumanMessage, SystemMessage
+# from langchain_openai import chat_models
+# from langchain import llms
 
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = "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"
-os.environ["OPENAI_API_KEY"] = "sk-proj-MHpGTFRaPeNFTaEaGWHFT3BlbkFJABUMjlLmcKTWa5th45rm"
+# os.environ["LANGCHAIN_TRACING_V2"] = "true"
+# os.environ["LANGCHAIN_API_KEY"] = "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"
+# os.environ["OPENAI_API_KEY"] = "sk-proj-OD1PVkNfYWOuVcXtbjFvT3BlbkFJVNuE1NaQIp441LPAwX0t"
 
-model = chat_models.ChatOpenAI(model="gpt-4")
-messages = [
-    (
-        "system",
-        "You are a helpful translator. Translate the user sentence to French.",
-    ),
-    ("human", "I love programming."),
-]
-print(model.invoke(messages))
+# # model = chat_models.ChatOpenAI(model="gpt-4o-mini")
+# model = llms
+# messages = [
+#     (
+#         "system",
+#         "You are a helpful translator. Translate the user sentence to French.",
+#     ),
+#     ("human", "I love programming."),
+# ]
+# print(model.invoke(messages))
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3.1-8B")
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3.1-8B")
+
+print(model)
