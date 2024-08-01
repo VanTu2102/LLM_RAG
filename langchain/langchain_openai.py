71765a6e5b8504305d7519600f2aeb36c4d336a0
Hoàng Tú <vantu2102@gmail.com>
2024-08-01 16:07:48 +0700

    .

--- langchain/langchain_openai.py
+++ langchain/langchain_openai.py
@@ -5,8 +5,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = ""
-os.environ["OPENAI_API_KEY"] = ""
+os.environ["LANGCHAIN_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "sk-proj-PvEuxmAlrEx6gAeXuTORT3BlbkFJYHP8DZGLgPfEy8tZXOlu"
 
 model = ChatOpenAI(model="gpt-3.5-turbo-1106")
 
